aa3774d725e2fa555f71bfef281f130ddd8703c0
weslley matos <weslley_mattosibf@hotmail.com>
2022-10-03 12:18:41 -0300

    change pages

--- programmingacademy/web/src/pages/profile/book/index.tsx
+++ programmingacademy/web/src/pages/profile/book/index.tsx
@@ -3,8 +3,19 @@ import { Pages } from "../../../Enums/navbar";
 
 export default function Book() {
     return (
-        <div className="bg-[url('./background9.jpg')] h-screen">
+        <div className="bg-background h-screen">
             <Navbar pageActive={Pages.profileBook} />
+
+            <div className='h-full flex justify-center items-center'>
+
+                <main className='h-[70%] w-[70%] rounded mb-10 px-12 bg-neutral-800 flex flex-col items-center justify-center'>
+
+                    <h1 className="text-4xl text-zinc-100 font-bold animate-bounce">Em desenvolvimento...</h1>
+
+                </main>
+
+            </div>
+
         </div>
     )
 }
